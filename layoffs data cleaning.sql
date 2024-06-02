f1d06b6adf6ecb8c0ca4849fc286108cc21041ee
Adonay G <adoghiwot901@gmail.com>
2024-06-02 12:38:16 +0300

    Update layoffs data cleaning.sql

--- layoffs data cleaning.sql
+++ layoffs data cleaning.sql
@@ -31,7 +31,7 @@ FROM (
                PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
            ) AS row_num
     FROM 
-        world_layoffs.layoffs_staging
+        layoffs_staging
 ) duplicates
 WHERE 
     row_num > 1;
